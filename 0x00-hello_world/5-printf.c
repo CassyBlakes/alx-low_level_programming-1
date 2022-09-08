dc8b7b9a8244929305932cc84438fffca6496ecb
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2022-09-08 10:17:57 -0700

    Print a C string using a printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The C function prints out the following string
+ * with proper grammar, but the outcome is a piece of art,
+ * Return: This function returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
