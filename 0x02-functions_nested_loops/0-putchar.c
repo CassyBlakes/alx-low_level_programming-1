b826ee1cc96479f7a5ef37493c354436d220ad7e
TheKhafre <105213608+TheKhafre@users.noreply.github.com>
2022-09-13 18:21:25 +0100

    updated error from return

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * main - print Putchar
- *
  * Return: Always 0 
  */
 int main(void)
@@ -15,5 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	return(0);
 }
