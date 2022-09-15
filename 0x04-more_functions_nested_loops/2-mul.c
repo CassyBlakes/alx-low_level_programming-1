275a5182d2220bf814f2e0a959df25b95fe0c055
TheKhafre <105213608+TheKhafre@users.noreply.github.com>
2022-09-15 16:49:21 +0100

    created the next few tasks according to understanding

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -this function multiplies two integers
+ * @a: multiplied with b
+ * @b: multiplied with a
+ * Return: this returns the multiplied value in a new variable c
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - this function prints the numbers 0 to 9
+ * Return: the program returns nothing
+ */
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
